c6c51ce62349032bc4614716cfd2f94833ada2ef
王诛魔 <wangzhumoo@gmail.com>
2019-04-01 23:02:55 +0800

    [feature]添加了module_jni

---
+++ module_jni/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_wangzhumo_app_module_jni_CppActivity_stringFromJNI(JNIEnv *env, jobject instance) {
+
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
---
+++ module_jni/src/main/java/com/wangzhumo/app/module/jni/CppActivity.java
@@ -0,0 +1,25 @@
+package com.wangzhumo.app.module.jni;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CppActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cpp);
+    }
+
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
