7cb2aebe0d35d2108d4dd367a89bf21ab7bd519b
王诛魔 <wangzhumoo@gmail.com>
2019-07-01 10:53:07 +0800

    [feature]测试

--- module_playground/src/main/java/com/wangzhumo/app/playground/FrameLayoutActivity.kt
+++ module_playground/src/main/java/com/wangzhumo/app/playground/FrameLayoutActivity.kt
@@ -1,5 +1,7 @@
 package com.wangzhumo.app.playground
 
+import android.media.MediaPlayer
+import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.wangzhumo.app.base.IRoute
 import com.wangzhumo.app.origin.BaseActivity
@@ -8,4 +10,14 @@ import com.wangzhumo.app.origin.BaseActivity
 @Route(path = IRoute.FRAME_ACTIVITY)
 class FrameLayoutActivity : BaseActivity() {
     override fun getLayoutId(): Int = R.layout.activity_fream_layout
+
+    override fun initData(savedInstanceState: Bundle?) {
+        super.initData(savedInstanceState)
+        var mediaPlayer: MediaPlayer = MediaPlayer()
+        mediaPlayer.setDataSource("/storage/emulated/0/Android/data/io.liuliu.music/files/audio/e968c8b31b2d51809936e60722892ad9_res/yinyu_audios_part7/warfare_bgm.m4a")
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            mediaPlayer.start()
+        }
+    }
 }
