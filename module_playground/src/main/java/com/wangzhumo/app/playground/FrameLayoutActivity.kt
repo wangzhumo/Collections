6259b03af9689699b2b82886620980c649189fd1
wangzhumo <wangzhumoo@gmail.com>
2019-07-26 14:23:10 +0800

    [feature]update

--- module_playground/src/main/java/com/wangzhumo/app/playground/FrameLayoutActivity.kt
+++ module_playground/src/main/java/com/wangzhumo/app/playground/FrameLayoutActivity.kt
@@ -1,23 +1,19 @@
 package com.wangzhumo.app.playground
 
-import android.media.MediaPlayer
 import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.wangzhumo.app.base.IRoute
 import com.wangzhumo.app.origin.BaseActivity
+import kotlinx.android.synthetic.main.activity_fream_layout.*
 
 
 @Route(path = IRoute.FRAME_ACTIVITY)
 class FrameLayoutActivity : BaseActivity() {
     override fun getLayoutId(): Int = R.layout.activity_fream_layout
 
-    override fun initData(savedInstanceState: Bundle?) {
-        super.initData(savedInstanceState)
-        var mediaPlayer: MediaPlayer = MediaPlayer()
-        mediaPlayer.setDataSource("/storage/emulated/0/Android/data/io.liuliu.music/files/audio/e968c8b31b2d51809936e60722892ad9_res/yinyu_audios_part7/warfare_bgm.m4a")
-        mediaPlayer.prepareAsync()
-        mediaPlayer.setOnPreparedListener {
-            mediaPlayer.start()
-        }
+
+    override fun initViews(savedInstanceState: Bundle?) {
+        super.initViews(savedInstanceState)
+        game_volume.startFakeAnim()
     }
 }
--- module_playground/src/main/java/com/wangzhumo/app/playground/GameVolumeView.java
+++ module_playground/src/main/java/com/wangzhumo/app/playground/GameVolumeView.java
@@ -3,26 +3,37 @@ package com.wangzhumo.app.playground;
 import android.content.Context;
 import android.graphics.*;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
-import android.view.View;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
  *
  * @author 王诛魔 2019-06-28  17:52
  */
-public class GameVolumeView extends View {
+public class GameVolumeView extends AppCompatImageView {
 
     private Bitmap mBitmap;
     private Paint mPaint;
-    private Rect mRect;
+    private Rect mDestRect;
+    private float mProgress;
+    private int mHeight;
+
+    private Disposable timeDisposable;
 
     public GameVolumeView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public GameVolumeView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs , 0);
+        this(context, attrs, 0);
     }
 
     public GameVolumeView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -33,24 +44,79 @@ public class GameVolumeView extends View {
 
     /**
      * 加载到Drawable
+     *
      * @param context ctx
-     * @param attrs attrs
+     * @param attrs   attrs
      */
     private void initViews(Context context, AttributeSet attrs) {
-        mBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.game_ic_mic_alphe);
+        mBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.game_ic_mic_yellow);
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
         mPaint.setDither(true);
-        mRect = new Rect(0,0,mBitmap.getWidth(),mBitmap.getHeight());
+        mHeight = mBitmap.getHeight();
+        mDestRect = new Rect(0, 0, mBitmap.getWidth(), mBitmap.getHeight());
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        PorterDuffXfermode xfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_IN);
-        canvas.drawRect(mRect,mPaint);
-        mPaint.setXfermode(xfermode);
-        mPaint.setColor(Color.YELLOW);
-        canvas.drawBitmap(mBitmap,0,0,mPaint);
+        canvas.save();
+        canvas.clipRect(mDestRect);
+        canvas.drawBitmap(mBitmap, 0, 0, mPaint);
+        canvas.restore();
     }
+
+
+    private long lastInvalidate;
+
+    /**
+     * 设置音波
+     *
+     * @param volume volume
+     */
+    public void setVolume(int volume) {
+        //决定是否开始更新
+        if (lastInvalidate + 20 > System.currentTimeMillis()) {
+            return;
+        }
+        lastInvalidate = System.currentTimeMillis();
+        if (volume > 150) {
+            mProgress = 0F;
+        } else {
+            mProgress = 1F - (volume / 150F);
+        }
+        mDestRect.top = (int) (mHeight * mProgress);
+        invalidate();
+    }
+
+    /**
+     * 开始一些模拟的动画
+     */
+    public void startFakeAnim() {
+        if (timeDisposable != null && !timeDisposable.isDisposed()) {
+            timeDisposable.dispose();
+            timeDisposable = null;
+        }
+        Random random = new Random();
+        timeDisposable = Observable.interval(10, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        setVolume(random.nextInt(150) + 1);
+                    }
+                });
+
+    }
+
+    /**
+     * 停止动画
+     */
+    public void release() {
+        if (timeDisposable != null && !timeDisposable.isDisposed()) {
+            timeDisposable.dispose();
+            timeDisposable = null;
+        }
+    }
+
 }
