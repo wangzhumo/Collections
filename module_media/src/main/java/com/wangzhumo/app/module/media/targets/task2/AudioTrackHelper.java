aaf12b9b9af05ad87292d67b4f4b5372368ba537
wangzhumo <wangzhumoo@gmail.com>
2019-08-23 00:39:20 +0800

    [feature]AudioTrackHelper

---
+++ module_media/src/main/java/com/wangzhumo/app/module/media/targets/task2/AudioTrackHelper.java
@@ -0,0 +1,15 @@
+package com.wangzhumo.app.module.media.targets.task2;
+
+import android.media.AudioTrack;
+
+/**
+ * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
+ *
+ * @author 王诛魔 2019-08-23  00:38
+ *
+ * 使用AudioTrack播放PCM文件
+ */
+public class AudioTrackHelper {
+
+    private AudioTrack mAudioTrack;
+}
