fb06589fbd27ecae8c7196604b6c6f175465d2a0
wangzhumo <wangzhumoo@gmail.com>
2019-08-22 21:45:32 +0800

    [feature]task 3

--- module_media/src/main/java/com/wangzhumo/app/module/media/targets/task1/Task2Activity.kt
+++ module_media/src/main/java/com/wangzhumo/app/module/media/targets/task1/Task2Activity.kt
@@ -1,9 +1,10 @@
 package com.wangzhumo.app.module.media.targets.task1
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import com.wangzhumo.app.module.media.R
 import com.wangzhumo.app.origin.BaseActivity
+import kotlinx.android.synthetic.main.activity_task2.*
 
 
 /**
@@ -11,9 +12,38 @@ import com.wangzhumo.app.origin.BaseActivity
  *
  * @author 王诛魔 2019-08-22  21:36
  *
+ * Task2:
+ *    AudioRecord & AudioTrack
  *
  */
 class Task2Activity : BaseActivity() {
     override fun getLayoutId(): Int = R.layout.activity_task2
 
+    override fun initViews(savedInstanceState: Bundle?) {
+        super.initViews(savedInstanceState)
+        button_record.setOnClickListener {
+            if (TextUtils.equals(button_record.text,"Record")){
+                button_record.text = "Stop"
+                startRecord()
+            }else{
+                button_record.text = "Record"
+                stopRecord()
+            }
+        }
+    }
+
+
+    /**
+     * 停止录制
+     */
+    private fun stopRecord() {
+        //
+    }
+
+    /**
+     * 开始录制
+     */
+    private fun startRecord() {
+        //
+    }
 }
