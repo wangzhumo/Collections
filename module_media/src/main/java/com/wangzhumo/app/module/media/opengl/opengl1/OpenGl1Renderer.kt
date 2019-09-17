cfd97f27621dd9d46306b2659e127f0c1dab648d
wangzhumo <wangzhumoo@gmail.com>
2019-09-17 19:52:38 +0800

    [feature]

--- module_media/src/main/java/com/wangzhumo/app/module/media/opengl/opengl1/OpenGl1Renderer.kt
+++ module_media/src/main/java/com/wangzhumo/app/module/media/opengl/opengl1/OpenGl1Renderer.kt
@@ -11,7 +11,7 @@ import javax.microedition.khronos.opengles.GL10
  *
  * opengl1的自定义渲染器.
  */
-class OpenGl1Renderer : GLSurfaceView.Renderer{
+class OpenGl1Renderer : GLSurfaceView.Renderer {
 
     //surface创建成功时
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
@@ -21,16 +21,20 @@ class OpenGl1Renderer : GLSurfaceView.Renderer{
 
     //size改变时回调
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
-
+        gl?.apply {
+            glViewport(0, 0, width, height)   //viewport视口   也就是opengl可以显示的区域大小
+            val ratio = width.toFloat() / height
+            //此处,  bottom = -left * ratio    top = left * ratio
+            //zNear 近平面距离
+            //zFar  远平面距离
+            glFrustumf(-1F, 1F, -ratio, ratio, 3F, 7F)
+        }
     }
 
     //绘制资源
     override fun onDrawFrame(gl: GL10?) {
 
     }
-
-
-
 
 
 }
