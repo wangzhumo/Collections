d1bbe480d4af1640c80357dc95daab2404c8e4ec
wangzhumo <wangzhumoo@gmail.com>
2019-09-28 18:38:21 +0800

    [feature] add AppDelegateFactory

---
+++ module_base/src/main/java/com/wangzhumo/app/base/delegate/AppDelegateFactory.java
@@ -0,0 +1,9 @@
+package com.wangzhumo.app.base.delegate;
+
+/**
+ * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
+ *
+ * @author 王诛魔 2019-09-28  18:12
+ */
+public class AppDelegateFactory {
+}
