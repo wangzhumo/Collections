8ed5aa021b63e7fc33e612cafda19e1279f30574
wangzhumo <wangzhumoo@gmail.com>
2019-09-28 18:39:14 +0800

    [feature] 添加注解

--- module_base/src/main/java/com/wangzhumo/app/base/delegate/IAppConstant.java
+++ module_base/src/main/java/com/wangzhumo/app/base/delegate/IAppConstant.java
@@ -1,11 +1,18 @@
 package com.wangzhumo.app.base.delegate;
 
+import androidx.annotation.StringDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
  *
  * @author 王诛魔 2019-09-28  17:50
  */
-public interface IAppConstant {
+@Retention(RetentionPolicy.SOURCE)
+@StringDef({IAppConstant.WEBRTC,IAppConstant.MEDIA})
+public @interface IAppConstant {
     String MEDIA = "app_media";
     String WEBRTC = "app_webrtc";
 }
