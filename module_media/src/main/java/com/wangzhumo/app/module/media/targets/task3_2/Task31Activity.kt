b66cff8b94663d63759aae1fc7298a7dcb54f6bf
wangzhumo <wangzhumoo@gmail.com>
2019-09-29 17:03:13 +0800

    [feature]加入TextureView

--- module_media/src/main/java/com/wangzhumo/app/module/media/targets/task3_2/Task31Activity.kt
+++ module_media/src/main/java/com/wangzhumo/app/module/media/targets/task3_2/Task31Activity.kt
@@ -1,18 +1,38 @@
 package com.wangzhumo.app.module.media.targets.task3_2
 
 import android.os.Bundle
+import android.view.TextureView
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.wangzhumo.app.base.IRoute
 import com.wangzhumo.app.module.media.R
 import com.wangzhumo.app.origin.BaseActivity
+import kotlinx.android.synthetic.main.activity_task31.*
 
-@Route(path = IRoute.MEDIA_TASK_3)
+
+/**
+ * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
+ *
+ * @author 王诛魔 2019-09-29  14:50
+ */
+@Route(path = IRoute.MEDIA_TASK_3_1)
 class Task31Activity : BaseActivity() {
+
+    lateinit var mTextureView: TextureView
+
+    lateinit var mCameraPick: CameraV1Pick
 
     override fun getLayoutId(): Int = R.layout.activity_task31
 
     override fun initViews(savedInstanceState: Bundle?) {
         super.initViews(savedInstanceState)
+        mTextureView = viewFinder
+        mCameraPick = CameraV1Pick()
+        mCameraPick.bindTextureView(mTextureView)
     }
 
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mCameraPick.onDestroy()
+    }
 }
