80c5a133e81bffa072b7592e6e82d7a2b819f009
wangzhumo <wangzhumoo@gmail.com>
2019-11-26 16:02:59 +0800

    [feature]为opengl添加注释 -3

--- module_media/src/main/res/raw/fragment_texture_shader.glsl
+++ module_media/src/main/res/raw/fragment_texture_shader.glsl
@@ -1,10 +1,18 @@
 #version 300 es
-#extension GL_OES_EGL_image_external_essl3 : require
-//使用OES纹理扩展
 
-precision mediump float;  //精度声明
-uniform samplerExternalOES uTextureSampler;  //OES纹理，接收相机的纹理作为输入
-in vec2 vTextureCoord;  //顶点着色器输入经图元装配和栅格化后的纹理坐标序列
+//使用OES纹理扩展
+#extension GL_OES_EGL_image_external_essl3 : require
+
+//精度声明
+precision mediump float;
+
+//OES纹理，接收相机的纹理作为输入
+uniform samplerExternalOES uTextureSampler;
+
+//顶点着色器输入经图元装配和栅格化后的纹理坐标序列,Vertex Shader中传递过来的 aTextureCoord
+in vec2 vTextureCoord;
+
+//最终告诉OpenGL要画的顶点颜色
 out vec4 vFragColor;
 
 void main()
--- module_media/src/main/res/raw/vertex_texture_shader.glsl
+++ module_media/src/main/res/raw/vertex_texture_shader.glsl
@@ -1,10 +1,16 @@
 #version 300 es
 
-layout (location = 0) in vec4 aPosition;       //NDC坐标点 ？？？
-layout (location = 1) in vec4 aTextureCoord;   //纹理坐标点
+//顶点数据，代表了要画的每个顶点,每画一个点就会调用一次，a_Position就代表当前要画的点
+layout (location = 0) in vec4 aPosition;
 
-uniform mat4 uTextureMatrix;  //纹理坐标变换矩阵
-out vec2 vTextureCoord;       //纹理坐标变换后的输出
+//纹理坐标数据，用于描述要画的纹理顶点
+layout (location = 1) in vec4 aTextureCoord;
+
+//纹理坐标变换矩阵
+uniform mat4 uTextureMatrix;
+
+//纹理坐标变换后的输出,用于将Vertex Shader中接受到的纹理顶点数据传递到Fragment Shader中
+out vec2 vTextureCoord;
 
 void main() {
      gl_Position  = aPosition;          //gl_position
