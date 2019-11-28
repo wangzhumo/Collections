70554e581d3538edd5ad828030fac39fb20b3d8f
wangzhumo <wangzhumoo@gmail.com>
2019-11-28 21:25:03 +0800

    [feature]添加入口

--- module_base/src/main/java/com/wangzhumo/app/base/IRoute.java
+++ module_base/src/main/java/com/wangzhumo/app/base/IRoute.java
@@ -25,6 +25,7 @@ public interface IRoute {
     String MEDIA_TASK_3_2 = "/media/activity/task_3_2";
     String MEDIA_OPENGL_1 = "/media/activity/opengl_1";
     String MEDIA_OPENGL_CAMERA = "/media/activity/opengl_camera";
+    String MEDIA_OPENGL_TRIANGLE = "/media/activity/opengl_triangle";
 
     String FRAME_ACTIVITY = "/activity/framelayout";
 
