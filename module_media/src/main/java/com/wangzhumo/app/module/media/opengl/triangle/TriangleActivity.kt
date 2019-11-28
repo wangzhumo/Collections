a3b774f2aecd78f04867291155407ad69ae18198
wangzhumo <wangzhumoo@gmail.com>
2019-11-28 21:24:20 +0800

    [feature]new TriangleActivity.kt

---
+++ module_media/src/main/java/com/wangzhumo/app/module/media/opengl/triangle/TriangleActivity.kt
@@ -0,0 +1,12 @@
+package com.wangzhumo.app.module.media.opengl.triangle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TriangleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_triangle)
+    }
+}
