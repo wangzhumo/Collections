25dff1e2721697429953860abf3498c3152aaca8
wangzhumo <wangzhumoo@gmail.com>
2019-11-28 21:24:52 +0800

    [feature]三角形

--- module_media/src/main/java/com/wangzhumo/app/module/media/opengl/triangle/TriangleActivity.kt
+++ module_media/src/main/java/com/wangzhumo/app/module/media/opengl/triangle/TriangleActivity.kt
@@ -1,12 +1,24 @@
 package com.wangzhumo.app.module.media.opengl.triangle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.wangzhumo.app.base.IRoute
+import com.wangzhumo.app.module.media.R
+import com.wangzhumo.app.origin.BaseActivity
 
-class TriangleActivity : AppCompatActivity() {
+/**
+ * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
+ *
+ * @author 王诛魔 2019-11-28  17:07
+ *
+ * 使用opengl绘制一个三角形
+ */
+@Route(path = IRoute.MEDIA_OPENGL_TRIANGLE)
+class TriangleActivity : BaseActivity() {
+    override fun getLayoutId(): Int = R.layout.activity_triangle
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_triangle)
+    override fun initViews(savedInstanceState: Bundle?) {
+
     }
+
 }
