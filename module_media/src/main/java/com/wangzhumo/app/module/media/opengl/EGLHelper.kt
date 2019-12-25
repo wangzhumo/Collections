abcf566bfb1f9c38491e0c231bc26408e8904c37
wangzhumo <wangzhumoo@gmail.com>
2019-12-25 20:59:04 +0800

    [feature]EGLHelper 帮助类

---
+++ module_media/src/main/java/com/wangzhumo/app/module/media/opengl/EGLHelper.kt
@@ -0,0 +1,11 @@
+package com.wangzhumo.app.module.media.opengl
+
+/**
+ * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
+ *
+ * @author 王诛魔 2019-12-25  19:32
+ *
+ *
+ */
+class EGLHelper {
+}
