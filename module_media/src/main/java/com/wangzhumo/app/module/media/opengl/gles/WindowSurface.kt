a066c87da5fd5d517f68b335fe53cde35bda963d
wangzhumo <wangzhumoo@gmail.com>
2019-12-31 16:26:58 +0800

    [feature]完成EglSurfaceBase

---
+++ module_media/src/main/java/com/wangzhumo/app/module/media/opengl/gles/WindowSurface.kt
@@ -0,0 +1,11 @@
+package com.wangzhumo.app.module.media.opengl.gles
+
+/**
+ * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
+ *
+ * @author 王诛魔 2019-12-31  16:16
+ *
+ *
+ */
+class WindowSurface {
+}
