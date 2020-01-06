e8fd89f4a35593dcf19806d66c01596af5789a5f
wangzhumo <wangzhumoo@gmail.com>
2020-01-06 15:06:38 +0800

    [feature]opengl模块的展示

--- module_opengl/src/main/java/com/wangzhumo/app/module/opengl/OpenGLActivity.kt
+++ module_opengl/src/main/java/com/wangzhumo/app/module/opengl/OpenGLActivity.kt
@@ -1,12 +1,50 @@
 package com.wangzhumo.app.module.opengl
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.launcher.ARouter
+import com.drakeet.multitype.MultiTypeAdapter
+import com.wangzhumo.app.base.IRoute
+import com.wangzhumo.app.module.opengl.module.ActivityItem
+import com.wangzhumo.app.origin.BaseActivity
+import com.wangzhumo.app.widget.rv.OnItemActionListener
+import kotlinx.android.synthetic.main.activity_open_gl.*
 
-class OpenGLActivity : AppCompatActivity() {
+/**
+ * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
+ *
+ * @author 王诛魔 2020-01-02  16:54
+ *
+ * OpenGL Module的ListActivity
+ */
+@Route(path = IRoute.OPENGL_LIST)
+class OpenGLActivity : BaseActivity() ,OnItemActionListener{
+    override fun getLayoutId(): Int = R.layout.activity_open_gl
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_open_gl)
+
+    override fun initViews(savedInstanceState: Bundle?) {
+        super.initViews(savedInstanceState)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val mAdapter = MultiTypeAdapter()
+        val itemData = create()
+
+        mAdapter.register(ActivityItem::class,ActivityListBinder(this))
+        recyclerView.adapter = mAdapter
+        mAdapter.items = itemData
+        mAdapter.notifyDataSetChanged()
+    }
+
+
+    private fun create() : List<ActivityItem>{
+        return mutableListOf(
+            ActivityItem("JustShow","简单使用GLSurfaceView显示", IRoute.OPENGL.JUST_SHOW)
+        )
+    }
+
+    override fun onAction(what: Int, any: Any?) {
+        ARouter.getInstance()
+            .build((any as ActivityItem).path)
+            .navigation()
     }
 }
--- module_opengl/src/main/java/com/wangzhumo/app/module/opengl/module/ActivityItem.kt
+++ module_opengl/src/main/java/com/wangzhumo/app/module/opengl/module/ActivityItem.kt
@@ -1,11 +1,18 @@
 package com.wangzhumo.app.module.opengl.module
 
+import com.wangzhumo.app.base.IRoute
+import java.util.ArrayList
+
 /**
  * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
  *
  * @author 王诛魔 2020-01-02  17:03
  *
- *
+ * 数据类
  */
-class ActivityItem {
-}
+data class ActivityItem (
+
+    val title :String,  //标题
+    val desc : String,  //描述
+    val path : String   //path
+)
