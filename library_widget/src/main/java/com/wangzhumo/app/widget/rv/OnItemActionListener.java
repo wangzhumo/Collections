a9f04238128ae129a2a43eac632cb3b14ec27b3e
wangzhumo <wangzhumoo@gmail.com>
2020-01-06 15:04:53 +0800

    [feature]添加新的模块

---
+++ library_widget/src/main/java/com/wangzhumo/app/widget/rv/OnItemActionListener.java
@@ -0,0 +1,9 @@
+package com.wangzhumo.app.widget.rv;
+
+/**
+ * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
+ *
+ * @author 王诛魔 2020-01-02  17:27
+ */
+public class OnItemActionListener {
+}
---
+++ module_opengl/src/main/java/com/wangzhumo/app/module/opengl/module/ActivityItem.kt
@@ -0,0 +1,11 @@
+package com.wangzhumo.app.module.opengl.module
+
+/**
+ * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
+ *
+ * @author 王诛魔 2020-01-02  17:03
+ *
+ *
+ */
+class ActivityItem {
+}
