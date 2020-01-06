78622fece853b87f2e18ae21a0acd3b76d9ade50
wangzhumo <wangzhumoo@gmail.com>
2020-01-06 21:34:23 +0800

    [feature]尝试透明背景 -2

--- module_opengl/src/main/java/com/wangzhumo/app/module/opengl/justshow/JustShowActivity.kt
+++ module_opengl/src/main/java/com/wangzhumo/app/module/opengl/justshow/JustShowActivity.kt
@@ -53,7 +53,7 @@ class JustShowActivity : BaseActivity() ,SurfaceHolder.Callback {
                 DensityUtils.getScreenHeight(this))
             while (true){
 
-                GLES20.glClearColor(0F,0F,0F,1.0F)
+                GLES20.glClearColor(0F,0F,0F,0F)
                 GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
 
                 eglHelper.swapBuffers(eglSurface)
