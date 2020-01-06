48ec1f03686f284bdd5b53dd8bb935b1f51a325b
wangzhumo <wangzhumoo@gmail.com>
2020-01-06 21:23:45 +0800

    [feature]完成EglHelper

--- module_opengl/src/main/java/com/wangzhumo/app/module/opengl/justshow/JustShowActivity.kt
+++ module_opengl/src/main/java/com/wangzhumo/app/module/opengl/justshow/JustShowActivity.kt
@@ -2,11 +2,15 @@ package com.wangzhumo.app.module.opengl.justshow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.wangzhumo.app.base.IRoute
+import com.wangzhumo.app.module.opengl.R
+import com.wangzhumo.app.origin.BaseActivity
 
-class JustShowActivity : AppCompatActivity() {
+@Route(path = IRoute.OPENGL.JUST_SHOW)
+class JustShowActivity : BaseActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_just_show)
-    }
+
+    override fun getLayoutId(): Int = R.layout.activity_just_show
+
 }
