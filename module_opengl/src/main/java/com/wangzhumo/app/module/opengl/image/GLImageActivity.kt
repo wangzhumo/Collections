11364eb5993325957c673e4f0b7d6fea784e408e
wangzhumo <wangzhumoo@gmail.com>
2020-01-08 20:58:38 +0800

    [feature]完成GLImageActivity

--- module_opengl/src/main/java/com/wangzhumo/app/module/opengl/image/GLImageActivity.kt
+++ module_opengl/src/main/java/com/wangzhumo/app/module/opengl/image/GLImageActivity.kt
@@ -1,12 +1,17 @@
 package com.wangzhumo.app.module.opengl.image
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import com.wangzhumo.app.module.opengl.R
+import com.wangzhumo.app.origin.BaseActivity
 
-class GLImageActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_glimage)
-    }
+/**
+ * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
+ *
+ * @author 王诛魔 2020-01-08  17:53
+ *
+ * 显示一个图片纹理到OpenGL
+ */
+class GLImageActivity : BaseActivity() {
+    override fun getLayoutId(): Int = R.layout.activity_glimage
+
 }
