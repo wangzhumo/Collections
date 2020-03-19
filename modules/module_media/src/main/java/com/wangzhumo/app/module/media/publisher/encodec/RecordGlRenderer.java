5adbfd55b1472d1f9cc40b02cd8c35b58feff29a
wangzhumo <wangzhumoo@gmail.com>
2020-03-19 18:22:29 +0800

    [feature]add RecordGlRenderer/RecordMediaCodec

---
+++ modules/module_media/src/main/java/com/wangzhumo/app/module/media/publisher/encodec/RecordGlRenderer.java
@@ -0,0 +1,25 @@
+package com.wangzhumo.app.module.media.publisher.encodec;
+
+import com.wangzhumo.app.mdeia.gles.IGLRenderer;
+
+/**
+ * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
+ *
+ * @author 王诛魔 2020/3/19  6:22 PM
+ */
+public class RecordGlRenderer implements IGLRenderer {
+    @Override
+    public void onSurfaceCreate() {
+
+    }
+
+    @Override
+    public void onSurfaceChange(int width, int height) {
+
+    }
+
+    @Override
+    public void drawFrame() {
+
+    }
+}
---
+++ modules/module_media/src/main/java/com/wangzhumo/app/module/media/publisher/encodec/RecordMediaCodec.java
@@ -0,0 +1,9 @@
+package com.wangzhumo.app.module.media.publisher.encodec;
+
+/**
+ * If you have any questions, you can contact by email {wangzhumoo@gmail.com}
+ *
+ * @author 王诛魔 2020/3/19  6:21 PM
+ */
+public class RecordMediaCodec extends BaseMediaCodec {
+}
