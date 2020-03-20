63c3d02f2db69531754e63dcc1423361e698a9b8
wangzhumo <wangzhumoo@gmail.com>
2020-03-20 21:45:08 +0800

    [feature]添加RecordActivity.kt

---
+++ modules/module_media/src/main/java/com/wangzhumo/app/module/media/publisher/encodec/RecordActivity.kt
@@ -0,0 +1,12 @@
+package com.wangzhumo.app.module.media.publisher.encodec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record)
+    }
+}
