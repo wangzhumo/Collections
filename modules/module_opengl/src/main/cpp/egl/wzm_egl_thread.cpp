483152cd965dfb4ea320b3e75be5e69180713e7a
wangzhumo <wangzhumoo@gmail.com>
2020-08-15 18:11:43 +0800

    [feature]添加了ELG流程图 / pthread

---
+++ modules/module_opengl/src/main/cpp/egl/wzm_egl_thread.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wangzhumo on 2020/8/15.
+//
+
+#include "../include/egl/wzm_egl_thread.h"
---
+++ modules/module_opengl/src/main/cpp/include/egl/wzm_egl_thread.h
@@ -0,0 +1,15 @@
+//
+// Created by wangzhumo on 2020/8/15.
+//
+
+#ifndef COLLECTIONS_WZM_EGL_THREAD_H
+#define COLLECTIONS_WZM_EGL_THREAD_H
+
+#include <pthread.h>
+
+class WzmEglThread {
+
+};
+
+
+#endif //COLLECTIONS_WZM_EGL_THREAD_H
