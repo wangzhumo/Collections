8a5aa9576e9e76c8016c66ddf629aefc4922da88
wangzhumo <wangzhumoo@gmail.com>
2020-08-15 22:11:49 +0800

    [feature]通过回调的形式-增强便捷调用

--- modules/module_opengl/src/main/cpp/egl/wzm_egl_thread.cpp
+++ modules/module_opengl/src/main/cpp/egl/wzm_egl_thread.cpp
@@ -9,10 +9,10 @@ WzmEglThread::~WzmEglThread() = default;
 
 void *eglThreadCallBack(void *context) {
 
-    WzmEglThread *wzmEglThread = static_cast<WzmEglThread *>(context);
+    auto *wzmEglThread = static_cast<WzmEglThread *>(context);
     if (wzmEglThread != nullptr) {
         // 如果不为空,就在这里创建一个EGL的环境.
-        WzmEglHelper *pEglHelper = new WzmEglHelper();
+        auto *pEglHelper = new WzmEglHelper();
         // 这里一个循环,用来处理数据
         while (true) {
             // 处理 surfaceCreate
@@ -20,19 +20,26 @@ void *eglThreadCallBack(void *context) {
                 LOGE("elgThread call surfaceCreate");
                 wzmEglThread->isCreate = false;
                 pEglHelper->initEglEnv(wzmEglThread->pNativeWindow);
+                // 回调给外部.
+                wzmEglThread->onCreateCall(wzmEglThread->onCreateCtx);
+
             }
             // 在这里处理 surfaceChange 的事件
             if (wzmEglThread->isChange) {
                 LOGE("eglThread call surfaceChange");
                 wzmEglThread->isChange = false;
-                glViewport(0,0,wzmEglThread->surfaceWidth,wzmEglThread->surfaceHeight);
+                // 这里执行回调.
+                wzmEglThread->onChangeCall(wzmEglThread->surfaceWidth,wzmEglThread->surfaceHeight,wzmEglThread->onChangeCtx);
+                // 告知执行完毕
                 wzmEglThread->canStart = true;
             }
             // 这里就开始绘制 surfaceDraw
             if (wzmEglThread->canStart){
                 LOGD("eglThread draw");
-                glClearColor(1.0, 1.0, 0.0, 1.0);
-                glClear(GL_COLOR_BUFFER_BIT);
+                // 这里执行回调.
+                wzmEglThread->onDrawCall(wzmEglThread->onDrawCtx);
+//                glClearColor(1.0, 1.0, 0.0, 1.0);
+//                glClear(GL_COLOR_BUFFER_BIT);
                 pEglHelper->swapBuffers();
             }
             // 暂停 60 fps
@@ -43,7 +50,9 @@ void *eglThreadCallBack(void *context) {
             }
         }
     }
-    pthread_exit(&wzmEglThread->pEglThread);
+    //也可以调用这个
+    //pthread_exit(&(wzmEglThread->pEglThread));
+    return nullptr;
 }
 
 void WzmEglThread::onSurfaceCreate(EGLNativeWindowType windowType) {
@@ -61,5 +70,23 @@ void WzmEglThread::onSurfaceChange(int width, int height) {
     isChange = true;
     surfaceWidth = width;
     surfaceHeight = height;
+}
+
+void WzmEglThread::setCreateCallBack(OnCreateCall onCreate, void *context) {
+    this->onCreateCall = onCreate;
+    this->onCreateCtx = context;
+}
+
+void WzmEglThread::setChangeCallBack(OnChangeCall onChange,int width, int height, void *context) {
+    this->onChangeCall = onChange;
+    this->surfaceHeight = height;
+    this->surfaceWidth = width;
+    this->onChangeCtx = context;
+}
+
+
+void WzmEglThread::setDrawCallBack(OnDrawCall drawCall, void *context) {
+    this->onDrawCall = drawCall;
+    this->onDrawCtx = context;
 }
 
--- modules/module_opengl/src/main/cpp/include/egl/wzm_egl_thread.h
+++ modules/module_opengl/src/main/cpp/include/egl/wzm_egl_thread.h
@@ -17,14 +17,6 @@
 
 class WzmEglThread {
 
-public:
-    WzmEglThread();
-    ~WzmEglThread();
-
-    void onSurfaceCreate(EGLNativeWindowType windowType);
-
-    void onSurfaceChange(int width, int height);
-
 
 public:
     pthread_t pEglThread = -1;
@@ -37,6 +29,43 @@ public:
 
     int surfaceWidth = 0;
     int surfaceHeight = 0;
+
+    // 定义一个回调 surfaceCreate
+    typedef void(*OnCreateCall)(void *);
+    OnCreateCall onCreateCall;
+
+    // 保存create参数
+    void *onCreateCtx;
+
+    // 定义一个回调 surfaceChange
+    typedef void(*OnChangeCall)(int width,int height,void *);
+    OnChangeCall onChangeCall;
+
+    // 保持change的参数
+    void *onChangeCtx;
+
+    // 定义一个回调 surfaceDraw
+    typedef void(*OnDrawCall)(void *);
+    OnDrawCall onDrawCall;
+
+    // 保持draw的参数
+    void *onDrawCtx;
+
+
+
+public:
+    WzmEglThread();
+    ~WzmEglThread();
+
+    void onSurfaceCreate(EGLNativeWindowType windowType);
+
+    void onSurfaceChange(int width, int height);
+
+    void setCreateCallBack(OnCreateCall onCreate,void *context);
+
+    void setChangeCallBack(OnChangeCall onChange,int width, int height,void *context);
+
+    void setDrawCallBack(OnDrawCall onDraw,void *context);
 };
 
 
