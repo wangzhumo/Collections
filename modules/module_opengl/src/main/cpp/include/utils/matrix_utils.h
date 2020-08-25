620dc3452c28ad920cd2f6fbdf602e2d9ba9bedc
wangzhumo <wangzhumoo@gmail.com>
2020-08-25 23:53:07 +0800

    [feature]矩阵的旋转

--- modules/module_opengl/src/main/cpp/include/utils/matrix_utils.h
+++ modules/module_opengl/src/main/cpp/include/utils/matrix_utils.h
@@ -5,6 +5,7 @@
 #ifndef COLLECTIONS_MATRIX_UTILS_H
 #define COLLECTIONS_MATRIX_UTILS_H
 
+#include <math.h>
 
 // 单位矩阵初始化
 // 单位矩阵不会改变原来顶点坐标的数据。
@@ -20,5 +21,23 @@ static void initMatrix(float *matrix){
     }
 }
 
+// 搞一个旋转矩阵
+// 传入一个角度，生成一个旋转矩阵
+static void rotateMatrix(double angle,float *matrix){
+    // 先给他reset
+    //initMatrix(matrix);
+    // 因为计算的公式需要的是弧度，我们把这个角度转为弧度
+    angle = angle * (M_PI / 180.0f);
+    // cos   -sin 0   0
+    // sin   cos  0   0
+    // 0     0    1   0
+    // 0     0    0   1
+    // 那么就是说，要计算四个位置的值
+    matrix[0] = cos(angle);
+    matrix[1] = -sin(angle);
+    matrix[5] = sin(angle);
+    matrix[6] = cos(angle);
+    // 旋转矩阵完成。
+}
 
 #endif //COLLECTIONS_MATRIX_UTILS_H
