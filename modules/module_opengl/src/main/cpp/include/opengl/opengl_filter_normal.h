a9d6721397246fc4113aefc1a0898aadfa9b8b16
wangzhumo <wangzhumoo@gmail.com>
2020-08-28 21:48:19 +0800

    [feature] opengl_filter_normal

--- modules/module_opengl/src/main/cpp/include/opengl/opengl_filter_normal.h
+++ modules/module_opengl/src/main/cpp/include/opengl/opengl_filter_normal.h
@@ -5,4 +5,24 @@
 #ifndef COLLECTIONS_OPENGL_FILTER_NORMAL_H
 #define COLLECTIONS_OPENGL_FILTER_NORMAL_H
 
+
+#include "base_opengl.h"
+
+class OpenGLFilterNormal: public BaseOpenGl{
+
+public:
+    OpenGLFilterNormal();
+    ~OpenGLFilterNormal();
+
+
+    // 需要实现一个onCreate
+    void onSurfaceCreate();
+
+    // onChange
+    void onSurfaceChange(int width,int height);
+
+    // 开始绘制
+    void onSurfaceDraw();
+};
+
 #endif //COLLECTIONS_OPENGL_FILTER_NORMAL_H
