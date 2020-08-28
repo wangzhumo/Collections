41051a8f86593334c6952af659e586035e11036d
wangzhumo <wangzhumoo@gmail.com>
2020-08-28 21:52:45 +0800

    [feature]移除干扰项目

--- modules/module_opengl/src/main/java/com/wangzhumo/app/module/opengl/OpenGLAppDelegate.kt
+++ modules/module_opengl/src/main/java/com/wangzhumo/app/module/opengl/OpenGLAppDelegate.kt
@@ -2,10 +2,6 @@ package com.wangzhumo.app.module.opengl
 
 import android.app.Application
 import android.util.Log
-import com.bun.miitmdid.core.MainMdidSdk
-import com.bun.miitmdid.core.MdidSdkHelper
-import com.bun.miitmdid.interfaces.IIdentifierListener
-import com.bun.miitmdid.interfaces.IdSupplier
 import com.google.auto.service.AutoService
 import com.wangzhumo.app.base.delegate.AppDelegate
 import com.wangzhumo.app.base.delegate.IApp
@@ -18,16 +14,10 @@ import com.wangzhumo.app.base.delegate.IAppConstant
  */
 @IApp(name = IAppConstant.OPENGL)
 @AutoService(AppDelegate::class)
-class OpenGLAppDelegate: AppDelegate, IIdentifierListener {
+class OpenGLAppDelegate: AppDelegate {
 
     override fun init(application: Application) {
         Log.d("AppDelegate", "OpenGLAppDelegate = $application")
-        MdidSdkHelper.InitSdk(application,true,this)
 
     }
-
-    override fun OnSupport(p0: Boolean, p1: IdSupplier?) {
-        Log.d("AppDelegate", "OnSupport p0 = $p0   p1 = $p1")
-    }
-
 }
