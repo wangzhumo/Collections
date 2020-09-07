ee3cccb842857df4f442db968d3fd35f65682c96
wangzhumo <wangzhumoo@gmail.com>
2020-09-07 23:54:21 +0800

    [feature]使用指针，把id传递给外部

--- modules/module_opengl/src/main/cpp/include/utils/shader_utils.h
+++ modules/module_opengl/src/main/cpp/include/utils/shader_utils.h
@@ -32,24 +32,29 @@ static GLuint loadShader(int shaderType, const char *source){
 
 
 // 创建一个program
-static GLuint createProgram(const char *vertexSource, const char *fragmentSource){
-    GLuint vertexShader = loadShader(GL_VERTEX_SHADER,vertexSource);
-    LOGE("createProgram loadShader vertexShader = %d",vertexShader);
-    GLuint fragmentShader = loadShader(GL_FRAGMENT_SHADER,fragmentSource);
-    LOGE("createProgram loadShader fragmentShader = %d",fragmentShader);
+// 这里的指针，为了让外部自己管理这个program的生命周期
+static GLuint loadShader2Program(const char *vertexSource, const char *fragmentSource, GLuint *vertexShader, GLuint *fragmentShader){
+    GLuint vertex_Shader = loadShader(GL_VERTEX_SHADER,vertexSource);
+    LOGD("loadShader2Program loadShader vertexShader = %d",vertex_Shader);
+    GLuint fragment_Shader = loadShader(GL_FRAGMENT_SHADER,fragmentSource);
+    LOGD("loadShader2Program loadShader fragmentShader = %d",fragment_Shader);
     // 创建program
-    GLuint programId = glCreateProgram();
-    if (programId == 0) {
-        LOGE("createProgram baseProgramId = %d",programId);
+    GLuint program_Id = glCreateProgram();
+    LOGE("loadShader2Program baseProgramId = %d",program_Id);
+    if (program_Id == 0) {
+        LOGE("loadShader2Program baseProgramId = %d",program_Id);
         return false;
     }
 
     // 加载 vertex 以及 fragment
-    glAttachShader(programId,vertexShader);
-    glAttachShader(programId,fragmentShader);
+    glAttachShader(program_Id,vertex_Shader);
+    glAttachShader(program_Id,fragment_Shader);
     // 链接
-    glLinkProgram(programId);
-    return programId;
+    glLinkProgram(program_Id);
+
+    vertexShader = &vertex_Shader;
+    fragmentShader = &fragment_Shader;
+    return program_Id;
 }
 
 
