4b8227ac6dc7c35989b9979e571344d3c656cb96
wangzhumo <wangzhumoo@gmail.com>
2021-02-04 17:59:53 +0800

    [feature]写入base类 -2

--- app/src/main/java/com/wangzhumo/app/MainActivity.kt
+++ app/src/main/java/com/wangzhumo/app/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.wangzhumo.app
 
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.launcher.ARouter
+import com.jakewharton.rxbinding4.view.clicks
+import com.tbruyelle.rxpermissions3.RxPermissions
 import com.tencent.mars.xlog.Log
 import com.wangzhumo.app.base.IRoute
-import com.wangzhumo.app.origin.base.BaseBindingActivity
 import com.wangzhumo.app.databinding.ActivityMainBinding
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterActivityLaunchConfigs
+import com.wangzhumo.app.origin.base.BaseBindingActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 
 
 @Route(path = IRoute.APP_MAIN)
@@ -18,52 +22,46 @@ public class MainActivity : BaseBindingActivity<ActivityMainBinding>() {
 
     override fun initViews(savedInstanceState: Bundle?) {
         super.initViews(savedInstanceState)
-//        startActivity(
-//            FlutterActivity
-//                .withCachedEngine("wangzhumo_engine")
-//                .backgroundMode(FlutterActivityLaunchConfigs.BackgroundMode.transparent)
-//                .build(this)
-//        )
         ARouter.getInstance().build(IRoute.SOCKET_ACTIVITY).navigation()
         vBinding.root.postDelayed({ this@MainActivity.finish() }, 500)
-//        vBinding.btMedia.clicks()
-//            .subscribeOn(AndroidSchedulers.mainThread())
-//            .flatMap {
-//                RxPermissions(this).request(
-//                    Manifest.permission.CAMERA,
-//                    Manifest.permission.RECORD_AUDIO
-//                )
-//            }
-//            .subscribe {
-//                if (it) {
-//                    // 如果完成，则点击事件
-//                    ARouter.getInstance()
-//                        .build(IRoute.MEDIA_MAIN)
-//                        .navigation()
-//                } else {
-//                    Toast.makeText(this, "请授权", Toast.LENGTH_LONG).show()
-//                }
-//            }
-//
-//
-//        vBinding.btOpengl.clicks()
-//            .subscribeOn(AndroidSchedulers.mainThread())
-//            .flatMap {
-//                RxPermissions(this).request(
-//                    Manifest.permission.CAMERA,
-//                    Manifest.permission.RECORD_AUDIO
-//                )
-//            }
-//            .subscribe {
-//                if (it) {
-//                    // 如果完成，则点击事件
-//                    ARouter.getInstance()
-//                        .build(IRoute.OPENGL_LIST)
-//                        .navigation()
-//                } else {
-//                    Toast.makeText(this, "请授权", Toast.LENGTH_LONG).show()
-//                }
-//            }
+        vBinding.btMedia.clicks()
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .flatMap {
+                RxPermissions(this).request(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.RECORD_AUDIO
+                )
+            }
+            .subscribe {
+                if (it) {
+                    // 如果完成，则点击事件
+                    ARouter.getInstance()
+                        .build(IRoute.MEDIA_MAIN)
+                        .navigation()
+                } else {
+                    Toast.makeText(this, "请授权", Toast.LENGTH_LONG).show()
+                }
+            }
+
+
+        vBinding.btOpengl.clicks()
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .flatMap {
+                RxPermissions(this).request(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.RECORD_AUDIO
+                )
+            }
+            .subscribe {
+                if (it) {
+                    // 如果完成，则点击事件
+                    ARouter.getInstance()
+                        .build(IRoute.OPENGL_LIST)
+                        .navigation()
+                } else {
+                    Toast.makeText(this, "请授权", Toast.LENGTH_LONG).show()
+                }
+            }
 
     }
 
